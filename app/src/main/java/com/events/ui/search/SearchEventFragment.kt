caf8b9afc4c747c000357d4f6fb356ef83cec8de
kabir <kabiragamirzaewv@gmail.com>
2022-07-21 18:29:43 +0300

    init commit ---  refactor fragment search event

--- app/src/main/java/com/events/ui/search/SearchEventFragment.kt
+++ app/src/main/java/com/events/ui/search/SearchEventFragment.kt
@@ -1,15 +1,18 @@
 package com.events.ui.search
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.events.App
-import com.events.R
 import com.events.databinding.FragmentSearchEventBinding
 import com.events.model.search.Event
 import com.events.utill.PreferencesManager
+import java.util.*
+
 
 class SearchEventFragment : Fragment(), SearchContract.View {
 
@@ -32,6 +35,23 @@ class SearchEventFragment : Fragment(), SearchContract.View {
         presenterImpl = SearchPresenterImpl((view.context.applicationContext as App).dataManager)
         presenterImpl.attachView(this)
 
+        with(binding) {
+            toolbarSearch.editTextSearchEventName.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) = Unit
+
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) =
+                    Unit
+
+                override fun afterTextChanged(s: Editable) {
+
+                }
+            })
+        }
     }
 
     override fun onSearchEvent(event: ArrayList<Event>) {
