848a4db16a1726b86ec300fec485b4fc7048584b
kabir <kabiragamirzaewv@gmail.com>
2022-07-21 18:01:45 +0300

    init commit --- refactor search event init presenter

--- app/src/main/java/com/events/ui/search/SearchEventFragment.kt
+++ app/src/main/java/com/events/ui/search/SearchEventFragment.kt
@@ -5,22 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.events.App
 import com.events.R
 import com.events.databinding.FragmentSearchEventBinding
+import com.events.model.search.Event
+import com.events.utill.PreferencesManager
 
-class SearchEventFragment : Fragment() {
+class SearchEventFragment : Fragment(), SearchContract.View {
 
-    private var binding: FragmentSearchEventBinding? = null
-    private val _binding get() = binding!!
+    private lateinit var binding: FragmentSearchEventBinding
+    private lateinit var presenterImpl: SearchPresenterImpl
+    private lateinit var preferencesManager: PreferencesManager
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSearchEventBinding.inflate(inflater, container, false)
-        return _binding.root
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        preferencesManager = PreferencesManager(requireContext())
+
+        presenterImpl = SearchPresenterImpl((view.context.applicationContext as App).dataManager)
+        presenterImpl.attachView(this)
     }
+
+    override fun onSearchEvent(event: ArrayList<Event>) {
+
+    }
+
+    override fun progress(show: Boolean) {
+
+    }
+
+    override fun noConnection() {
+
+    }
+
 }
