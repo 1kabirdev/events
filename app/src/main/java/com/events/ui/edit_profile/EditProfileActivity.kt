67c558b42696d6429e5f8bbbbc08112dc6f98c7c
kabir <kabiragamirzaewv@gmail.com>
2022-07-22 19:29:56 +0300

    init commit ---  refactor code init presenter edit profile

--- app/src/main/java/com/events/ui/edit_profile/EditProfileActivity.kt
+++ app/src/main/java/com/events/ui/edit_profile/EditProfileActivity.kt
@@ -2,15 +2,21 @@ package com.events.ui.edit_profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.events.App
 import com.events.R
 import com.events.databinding.ActivityEditAccountBinding
+import com.events.model.profile.UpdateAvatar
 import com.squareup.picasso.Picasso
 
-class EditProfileActivity : AppCompatActivity() {
+class EditProfileActivity : AppCompatActivity(), EditProfileController.View {
+
     private var avatar: String? = null
     private var username: String? = null
     private var lastName: String? = null
     private var about: String? = null
+
+    private lateinit var presenter: EditProfilePresenter
+
     private lateinit var binding: ActivityEditAccountBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +29,9 @@ class EditProfileActivity : AppCompatActivity() {
         lastName = args?.get("LASTNAME").toString() ?: "default"
         about = args?.get("ABOUT").toString() ?: "default"
 
+        presenter = EditProfilePresenter((applicationContext as App).dataManager)
+        presenter.attachView(this)
+
         with(binding) {
             btnBack.setOnClickListener { finish() }
             Picasso.get().load(avatar).into(avatarEditImageView)
@@ -30,8 +39,19 @@ class EditProfileActivity : AppCompatActivity() {
             editTextLastName.setText(lastName)
             editTextAbout.setText(about)
             btnSaveEdit.setOnClickListener {
-
             }
         }
+    }
+
+    override fun updateAvatar(updateAvatar: UpdateAvatar) {
+
+    }
+
+    override fun progressAvatar(show: Boolean) {
+
+    }
+
+    override fun errorAvatar() {
+
     }
 }
