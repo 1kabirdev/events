e049aa7e798d8e973d8a27ba3ad5c5ab20a6fb71
kabir <kabiragamirzaewv@gmail.com>
2022-07-22 18:57:58 +0300

    init commit ---  refactor code presenter edit profile

--- app/src/main/java/com/events/ui/edit_profile/EditProfilePresenter.kt
+++ app/src/main/java/com/events/ui/edit_profile/EditProfilePresenter.kt
@@ -1,4 +1,12 @@
 package com.events.ui.edit_profile
 
-class EditProfilePresenter {
+import com.events.data.DataManager
+import com.events.mvp.BasePresenter
+
+class EditProfilePresenter(private var dataManager: DataManager) :
+    BasePresenter<EditProfileController.View>(), EditProfileController.Presenter {
+
+    override fun responseUpdateAvatar(user_id: Int, image: ByteArray) {
+
+    }
 }
