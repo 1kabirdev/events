ca873157338a3a7a8cb8c752bf0bfe9a8d4f26da
kabir <kabiragamirzaewv@gmail.com>
2022-07-22 19:03:49 +0300

    init commit ---  edit presenter avatar fun response upload avatar

--- app/src/main/java/com/events/ui/edit_profile/EditProfilePresenter.kt
+++ app/src/main/java/com/events/ui/edit_profile/EditProfilePresenter.kt
@@ -1,12 +1,48 @@
 package com.events.ui.edit_profile
 
 import com.events.data.DataManager
+import com.events.model.profile.UpdateAvatar
 import com.events.mvp.BasePresenter
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EditProfilePresenter(private var dataManager: DataManager) :
     BasePresenter<EditProfileController.View>(), EditProfileController.Presenter {
 
-    override fun responseUpdateAvatar(user_id: Int, image: ByteArray) {
+    private lateinit var call: Call<UpdateAvatar>
 
+    override fun responseUpdateAvatar(user_id: Int, image: ByteArray) {
+        mvpView?.let { view ->
+            view.progressAvatar(true)
+            val tsLong = System.currentTimeMillis() / 1000
+            val nameImage = "$tsLong.jpg"
+            val requestFile = RequestBody.create("image/*".toMediaTypeOrNull(), image)
+            val imageUpload = MultipartBody.Part.createFormData("image", nameImage, requestFile)
+            val userId = user_id.toString().toRequestBody("text/plain".toMediaTypeOrNull())
+            call = dataManager.updateAvatar(userId, imageUpload)
+            call.enqueue(object : Callback<UpdateAvatar> {
+                override fun onResponse(
+                    call: Call<UpdateAvatar>,
+                    response: Response<UpdateAvatar>
+                ) {
+                    view.progressAvatar(false)
+                    if (response.isSuccessful) {
+                        response.body()?.let { data ->
+                            view.updateAvatar(data)
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<UpdateAvatar>, t: Throwable) {
+                    view.progressAvatar(false)
+                    view.errorAvatar()
+                }
+            })
+        }
     }
 }
