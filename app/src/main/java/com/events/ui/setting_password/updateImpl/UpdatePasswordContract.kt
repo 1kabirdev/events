1db856c815d7312955996702a1e70764600292b0
kabir <kabiragamirzaewv@gmail.com>
2022-07-25 19:39:08 +0300

    init commit --- refactor add contract update password --- 0.21

---
+++ app/src/main/java/com/events/ui/setting_password/updateImpl/UpdatePasswordContract.kt
@@ -0,0 +1,21 @@
+package com.events.ui.setting_password.updateImpl
+
+import com.events.model.profile.UpdatePassword
+import com.events.mvp.MvpView
+
+interface UpdatePasswordContract {
+    interface View : MvpView {
+        fun updatePassword(updatePassword: UpdatePassword)
+        fun progress(show: Boolean)
+        fun error()
+    }
+
+    interface Presenter : MvpView {
+        fun responseUpdatePassword(
+            user_id: Int,
+            old_password: String,
+            new_password: String,
+            conf_password: String
+        )
+    }
+}
---
+++ app/src/main/java/com/events/ui/setting_password/updateImpl/UpdatePasswordPresenterImpl.kt
@@ -0,0 +1,4 @@
+package com.events.ui.setting_password.updateImpl
+
+class UpdatePasswordPresenterImpl {
+}
