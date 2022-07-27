90fc4c50a3cb61552e8f3593dc8d6aadd7885215
kabir <kabiragamirzaewv@gmail.com>
2022-07-27 16:15:15 +0300

    init commit --- refactor code theme event init presenter --- 0.90

--- app/src/main/java/com/events/ui/theme_list/ThemeListActivity.kt
+++ app/src/main/java/com/events/ui/theme_list/ThemeListActivity.kt
@@ -2,17 +2,27 @@ package com.events.ui.theme_list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.events.App
 import com.events.databinding.ActivityThemeListBinding
+import com.events.model.theme_event.InfoEvents
+import com.events.model.theme_event.ListEvents
 import com.events.utill.ConstantAgrs
 import com.events.utill.PreferencesManager
 
-class ThemeListActivity : AppCompatActivity() {
+class ThemeListActivity : AppCompatActivity(), ThemeListEventContract.View {
 
     private lateinit var preferencesManager: PreferencesManager
     private lateinit var binding: ActivityThemeListBinding
+    private lateinit var presenter: ThemeListEventPresenter
 
     private var name: String = ""
     private var icons: String = ""
+
+    private var errorFailed = false
+    private var isLoading = false
+    private var isLastPage = false
+    private val PAGE_START = 1
+    private var currentPage: Int = PAGE_START
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +35,29 @@ class ThemeListActivity : AppCompatActivity() {
         name = args?.get(ConstantAgrs.NAME)?.toString().toString()
         icons = args?.get(ConstantAgrs.ICONS)?.toString().toString()
 
+        presenter = ThemeListEventPresenter((applicationContext as App).dataManager)
+        presenter.attachView(this)
+        presenter.responseThemeEvent(name, PAGE_START)
+
+    }
+
+    override fun loadEventTheme(info: InfoEvents, listEvents: ArrayList<ListEvents>) {
+
+    }
+
+    override fun loadEventThemePage(info: InfoEvents, listEvents: ArrayList<ListEvents>) {
+
+    }
+
+    override fun progress(show: Boolean) {
+
+    }
+
+    override fun error() {
+
+    }
+
+    override fun errorPage() {
 
     }
 }
