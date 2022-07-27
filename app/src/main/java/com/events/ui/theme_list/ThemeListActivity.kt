6f851837a55ffe80dc54ae968b61574b1e99e1e2
kabir <kabiragamirzaewv@gmail.com>
2022-07-27 16:13:04 +0300

    init commit --- refactor code theme event intent args --- 0.89

--- app/src/main/java/com/events/ui/theme_list/ThemeListActivity.kt
+++ app/src/main/java/com/events/ui/theme_list/ThemeListActivity.kt
@@ -3,6 +3,7 @@ package com.events.ui.theme_list
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.events.databinding.ActivityThemeListBinding
+import com.events.utill.ConstantAgrs
 import com.events.utill.PreferencesManager
 
 class ThemeListActivity : AppCompatActivity() {
@@ -10,11 +11,20 @@ class ThemeListActivity : AppCompatActivity() {
     private lateinit var preferencesManager: PreferencesManager
     private lateinit var binding: ActivityThemeListBinding
 
+    private var name: String = ""
+    private var icons: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityThemeListBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         preferencesManager = PreferencesManager(this)
+
+        val args = intent.extras
+        name = args?.get(ConstantAgrs.NAME)?.toString().toString()
+        icons = args?.get(ConstantAgrs.ICONS)?.toString().toString()
+
+
     }
 }
