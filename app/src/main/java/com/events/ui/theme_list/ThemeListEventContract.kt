c0e4500890d534dd21b90a7c34ac7c9ef56090d2
kabir <kabiragamirzaewv@gmail.com>
2022-07-28 16:31:45 +0300

    init commit --- refactor contract --- 0.105

--- app/src/main/java/com/events/ui/theme_list/ThemeListEventContract.kt
+++ app/src/main/java/com/events/ui/theme_list/ThemeListEventContract.kt
@@ -2,6 +2,7 @@ package com.events.ui.theme_list
 
 import com.events.model.theme_event.InfoEvents
 import com.events.model.theme_event.ListEvents
+import com.events.model.theme_event.Subscribe
 import com.events.mvp.MvpView
 
 interface ThemeListEventContract {
@@ -12,11 +13,12 @@ interface ThemeListEventContract {
         fun error()
         fun errorPage()
 
-        fun subscribe()
+        fun subscribe(subscribe: Subscribe)
     }
 
     interface Presenter : MvpView {
         fun responseThemeEvent(theme: String, page: Int)
         fun responseThemeEventPage(theme: String, page: Int)
+        fun responseSubscribe(user_id:Int,name_theme:String)
     }
 }
